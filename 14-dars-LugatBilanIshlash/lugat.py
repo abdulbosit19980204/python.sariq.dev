94849b45aa613f21e360496cc664cc48c6a3659c
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-21 23:41:20 +0500

    Lug'at bilan ishlash

---
+++ 14-dars-LugatBilanIshlash/lugat.py
@@ -0,0 +1,18 @@
+car_0 = {'model':'ferrari','rang':'qizil'}
+
+
+car_0 = {'model':'ferrari','rang':'qizil'}
+print(car_0['model'])
+
+print(car_0['rang'])
+
+
+talaba_0 = {'ism':'murod olimov','yosh':20,'t_yil':2000}
+print(f"{talaba_0['ism'].title()},\
+ {talaba_0['t_yil']}-yilda tu'gilgan,\
+ {talaba_0['yosh']} yoshda")
+
+talaba_0['kurs'] = 4 # yangi, 'kurs' nomli kalit so'zga 4 qiymatini yuklaymiz
+talaba_0['fakultet'] = 'informatika' # 'fakultet' ga esa 'informatika' 
+
+print(talaba_0)
